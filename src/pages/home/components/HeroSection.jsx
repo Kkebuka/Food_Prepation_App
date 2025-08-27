21c19e904f1626b057653d6f981578708bfa64c9
kkebuka <kkebuka@gmail.com>
2025-08-27 16:32:04 +0100

    minor change

--- src/pages/home/components/HeroSection.jsx
+++ src/pages/home/components/HeroSection.jsx
@@ -7,13 +7,13 @@ export default function HeroSection() {
         <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1551218808-94e220e084d2?q=80&w=2000&auto=format&fit=crop')] bg-cover bg-center" />
         <div className="absolute inset-0 bg-black/30" />
         <div className="relative">
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-2 sm:px-2 lg:px-8">
             <div className="py-14 sm:py-20 lg:py-28">
-              <div className="mx-auto max-w-3xl   ">
-                <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-4xl md:text-5xl">
+              <div className=" ">
+                <h1 className="text-4xl font-extrabold text-center tracking-tight text-white sm:text-4xl md:text-5xl">
                   Home of delicious recipe
                 </h1>
-                <p className="mt-4 text-base text-gray-100 sm:text-lg">
+                <p className="mt-4 text-base text-gray-100 text-center sm:text-lg">
                 Turn cooking into an unforgettable culinary adventure.
                 </p>
               </div>
